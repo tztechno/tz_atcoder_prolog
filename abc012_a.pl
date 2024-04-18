f323eb40d7696e73f041c8d9b9236770db44941b
stpeteishii <47345279+tztechno@users.noreply.github.com>
2024-04-18 17:18:33 +0900

    Update abc012_a.pl

--- abc012_a.pl
+++ abc012_a.pl
@@ -3,6 +3,18 @@
 
 ##########################################################
 ##########################################################
+main :-
+	read_num(A),
+        read_num(B),
+	write(B), write(' '), write(A),
+        nl.
+	
+read_str(S) :-
+        read_string(current_input, ' \n', '', _, S).
+
+read_num(N) :-
+	read_str(S),
+	number_string(N, S).
 ##########################################################
 :- use_module(library(clpfd)).
 
